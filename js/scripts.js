047c35db1c69a77fc1dac19453a01560c87e530b
Pawel Dudek <paweldudek1988@gmail.com>
2018-01-22 13:55:39 +0900

    Add file structures, Modify JS checkWinner function

---
+++ js/scripts.js
@@ -0,0 +1,152 @@
+// Wartości początkowe
+
+var gameState = 'notStarted',  //started // ended
+    player = {
+        name: '',
+        score: 0
+    },
+    computer = {
+        score: 0
+    };
+
+// New Game
+var newGameBtn = document.getElementById('js-newGameButton');
+
+newGameBtn.addEventListener('click', newGame);
+
+
+// Buttons VAR
+var  pickRock = document.getElementById('js-playerPick_rock'),
+     pickPaper = document.getElementById('js-playerPick_paper'),
+     pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+// Game results
+
+
+// Wyświetlanie elemtnów gry
+var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
+
+// Funcja dla gameState
+
+function setGameElements() {
+  switch(gameState) {
+    case 'started':
+        newGameElem.style.display = 'none';
+        pickElem.style.display = 'block';
+        resultsElem.style.display = 'block';
+      break;
+    case 'ended':
+        newGameBtn.innerText = 'Jeszcze raz';
+    case 'notStarted':
+    default:
+        newGameElem.style.display = 'block';
+        pickElem.style.display = 'none';
+        resultsElem.style.display = 'none';
+  }
+}
+
+// Rozpoczęcie gry
+
+setGameElements();
+
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+
+// New game / Play again
+
+function newGame() {
+  player.name = prompt('Please enter your name', 'imię gracza');
+  if (player.name) {
+    player.score = computer.score = 0;
+    gameState = 'started';
+    setGameElements();
+
+    playerNameElem.innerHTML = player.name;
+    setGamePoints();
+  }
+}
+
+// Wybór gracza
+
+function playerPick(playerPick) {
+    console.log(playerPick);
+}
+
+// Losowanie wyboru komputera
+
+function getComputerPick() {
+    var possiblePicks = ['rock', 'paper', 'scissors'];
+    return possiblePicks[Math.floor(Math.random()*3)];
+}
+
+var playerPickElem = document.getElementById('js-playerPick'),
+    computerPickElem = document.getElementById('js-computerPick'),
+    playerResultElem = document.getElementById('js-playerResult'),
+    computerResultElem = document.getElementById('js-computerResult');
+
+function playerPick(playerPick) {
+    var computerPick = getComputerPick();
+
+    playerPickElem.innerHTML = playerPick;
+    computerPickElem.innerHTML = computerPick;
+
+    checkRoundWinner(playerPick, computerPick);
+}
+
+// Przyznawanie punktów
+
+function checkRoundWinner(playerPick, computerPick) {
+    playerResultElem.innerHTML = computerResultElem.innerHTML = '';
+
+    var winnerIs = 'player';
+
+      if (playerPick == computerPick) {
+          winnerIs = 'noone'; // remis
+    } else if (
+          (computerPick == 'rock' &&  playerPick == 'scissors') ||
+          (computerPick == 'scissors' &&  playerPick == 'paper') ||
+          (computerPick == 'paper' &&  playerPick == 'rock')) {
+
+          winnerIs = 'computer';
+    }
+
+    if (winnerIs == 'player') {
+          playerResultElem.innerHTML = "Win!";
+          player.score++;
+    } else if (winnerIs == 'computer') {
+          computerResultElem.innerHTML = "Win!";
+          computer.score++;
+    }
+setGamePoints();
+
+}
+
+
+function setGamePoints() {
+    playerPointsElem.innerHTML = player.score;
+    computerPointsElem.innerHTML = computer.score;
+}
+
+//Nie dziala ↓↓↓
+function checkWinner() {
+
+  if (player.score == 2) {
+    return player.name
+    alert("Player wins");
+  }
+
+  else if (computer.score == 2) {
+    return computer
+    alert("Computer wins")
+  }
+}
+
+//Nie dziala ↑↑↑
